bff447808e7546177b43ef279111a798e22f8985
hspl-rohan <rokir@heliconia.in>
2024-06-20 12:53:35 +0530

    migrate to vite

--- app.js
+++ app.js
@@ -1,37 +1,15 @@
-const express = require("express")
-const collection = ("./mongo")
-const cors =require("cors")
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(cors())
+// const express = require("express")
+import { express } from "express";
+import { collection } from "./mongo";
+import cors from "cors";
+// const collection = ("./mongo")
+// const cors =require("cors")
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-
-
-app.post("/",async(req,res) => {
-    const{email,password}=req.body
-
-    const data={
-        emial:email,
-        password:password
-    }
-
-    try {
-        const check = await collection.findOne({email:email})
-
-        if(check){
-            res.json("exist")
-        }else{
-            res.json("not exist")
-            await collection.insertMany([data])
-        }
-
-    } catch (e) {
-        res.json("not exist")
-    }
-})
-
-app.post("/signup", async (req, res) => {
+app.post("/", async (req, res) => {
   const { email, password } = req.body;
 
   const data = {
@@ -53,6 +31,31 @@ app.post("/signup", async (req, res) => {
   }
 });
 
-app.listen(8000,()=>{
-    console.log("port connected")
-})
+app.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
+
+  const data = {
+    emial: email,
+    password: password,
+  };
+
+  try {
+    const check = await collection.findOne({ email: email });
+
+    if (check) {
+      res.json("exist");
+      console.log(res);
+    } else {
+      res.json("not exist");
+      console.log(res);
+      await collection.insertMany([data]);
+    }
+  } catch (e) {
+    res.json("not exist");
+    console.log(res);
+  }
+});
+
+app.listen(8000, () => {
+  console.log("port connected");
+});
